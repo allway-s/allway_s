1ba10ea216f68982d633a089c912fe4ee310974c
smithrowe-10 <smj0418smj@gmail.com>
2026-01-12 17:04:31 +0900

    <feat>(<global>): <order로직 만드는중>

--- allways_back/src/main/java/com/korit/allways_back/entity/OrderDetail.java
+++ allways_back/src/main/java/com/korit/allways_back/entity/OrderDetail.java
@@ -5,18 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+public class OrderDetail {
 
+    private int orderDetailId;
+    private int orderId;
     private int productId;
-    private int isSys;
-    private int itemId;
-    private List<Integer> ingredientIds;
-    private List<Ingredient> ingredients;
+    private int unitPrice;
+    private int quantity;
 
 }
--- allways_back/src/main/java/com/korit/allways_back/mapper/OrderMapper.java
+++ allways_back/src/main/java/com/korit/allways_back/mapper/OrderMapper.java
@@ -1,8 +1,18 @@
 package com.korit.allways_back.mapper;
 
+import com.korit.allways_back.entity.Order;
+import com.korit.allways_back.entity.OrderDetail;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
+    int insertOrder(Order order); // orderId가 여기 주입됨
 
+    int insertOrderDetails(
+            @Param("orderId") int orderId,
+            @Param("orderDetails") List<OrderDetail> orderDetails
+    );
 }
