0ab726ff7ee326e8a172cd3a484e542e8fd8577b
JINHYUN-BRIGHTDAY <jinibrighteveryday@gmail.com>
2026-01-12 09:17:03 +0900

    fix: resolve import conflict

--- allways_front/src/main.jsx
+++ allways_front/src/main.jsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PresetCard } from './components/PresetCard'
-import { HomePage } from './pages/MainPage//HomePage/index';
+import { HomePage } from './pages/MainPage/HomePage/index';
 import OrderPage from './pages/order/OrderPage';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
