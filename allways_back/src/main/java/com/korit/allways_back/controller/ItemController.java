e87896a6f3d34d848e07aa11994f0a6dc67e62d7
smithrowe-10 <smj0418smj@gmail.com>
2026-01-12 17:50:47 +0900

    <feat>(<global>): <swagger 함>

--- allways_back/src/main/java/com/korit/allways_back/controller/ItemController.java
+++ allways_back/src/main/java/com/korit/allways_back/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.korit.allways_back.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +16,8 @@ public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping
-    public ResponseEntity<List<Item>> getItems(String categoryName) {
+    @GetMapping("/api/items")
+    public ResponseEntity<List<Item>> getItems(@RequestParam("categoryName") String categoryName) {
         return ResponseEntity.ok(itemService.getItemByCategory(categoryName));
     }
 
