b5bac596765e8cd5a063fc598fc926f752d4ed5d
JINHYUN-BRIGHTDAY <jinibrighteveryday@gmail.com>
2026-01-12 09:14:41 +0900

    fix: merge conflict in main.jsx

--- allways_front/src/main.jsx
+++ allways_front/src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PresetCard } from './components/PresetCard'
-import { HomePage } from './pages/MainPage/HomePage/index';
-import App from './App';
-
+import { HomePage } from './pages/MainPage//HomePage/index';
+import OrderPage from './pages/order/OrderPage';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <OrderPage />
   </StrictMode>,
 )
