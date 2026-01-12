2ba2acaf440ff28e70970137459e4005623e3528
chlqorrbs <qorrbs9341@gmail.com>
2026-01-12 14:24:45 +0900

    feat : 메뉴 페이지 생성

--- allways_front/src/main.jsx
+++ allways_front/src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { PresetCard } from './components/PresetCard'
-import { HomePage } from './pages/HomePage'
-
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { PresetCard } from './components/PresetCard';
+import { HomePage } from './pages/HomePage';
+import { MenuPage } from './pages/menu/MenuPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MenuPage />
+  </StrictMode>
+);
