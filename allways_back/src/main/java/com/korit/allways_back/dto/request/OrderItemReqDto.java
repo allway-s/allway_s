859d060665c880249221ee3be2163568d850b9e1
smithrowe-10 <smj0418smj@gmail.com>
2026-01-14 12:36:27 +0900

    <style>(<global>): <order 기능 포맷팅>

--- allways_back/src/main/java/com/korit/allways_back/dto/request/OrderItemReqDto.java
+++ allways_back/src/main/java/com/korit/allways_back/dto/request/OrderItemReqDto.java
@@ -1,9 +1,12 @@
 package com.korit.allways_back.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -11,9 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OrderItemReqDto {
 
-    private int orderId;
-    private int productId;
-    private int unitPrice;
+    private int itemId;
+    private List<Integer> ingredientIds;
     private int quantity;
+
+    @Schema(hidden = true)
+    private int productId;
+
+    @Schema(hidden = true)
+    private int unitPrice;
 
 }
