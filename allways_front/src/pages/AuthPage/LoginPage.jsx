f3c83966826bb34e21709eecacdb8ba9eb5923ef
JINHYUN-BRIGHTDAY <jinibrighteveryday@gmail.com>
2026-01-16 15:17:16 +0900

    <style>(<globe>): <resolve merge conflict in LoginPage>

--- allways_front/src/pages/AuthPage/LoginPage.jsx
+++ allways_front/src/pages/AuthPage/LoginPage.jsx
@@ -1,36 +1,29 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { S } from './Login.styles.js';
 
-
 const Login = () => {
-  const navigate = useNavigate();
-
-  const handleOAuth2LoginOnClick = (e) => {
-    const clientName = e.target.id;
+  const handleOAuth2LoginOnClick = (clientName) => {
     window.location.href =
-      'http://localhost:8080/oauth2/authorization/' + clientName;
+      `http://localhost:8080/oauth2/authorization/${clientName}`;
   };
 
   return (
     <div css={S.container}>
-    
       <main css={S.card}>
         <h2 css={S.title}>LOGIN</h2>
 
         <div css={S.buttonGroup}>
           <button
-            id='naver'
             css={S.socialButton('naver')}
-            onClick={handleOAuth2LoginOnClick}
+            onClick={() => handleOAuth2LoginOnClick('naver')}
           >
             네이버 로그인
           </button>
           <button
-            id='google'
             css={S.socialButton('google')}
-            onClick={handleOAuth2LoginOnClick}
+            onClick={() => handleOAuth2LoginOnClick('google')}
           >
             구글 로그인
           </button>
