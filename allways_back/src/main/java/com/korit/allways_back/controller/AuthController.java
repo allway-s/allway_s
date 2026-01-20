c724fe89d85a2eb130955cf66e42d492f196fea4
HWANGDEOK <dukjun2002@naver.com>
2026-01-20 11:22:00 +0900

    feat(signup): 회원가입 즉시 토큰 발행 수정-백엔드

--- allways_back/src/main/java/com/korit/allways_back/controller/AuthController.java
+++ allways_back/src/main/java/com/korit/allways_back/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.korit.allways_back.controller;
 
 import com.korit.allways_back.dto.request.SignupRequestDto;
 import com.korit.allways_back.entity.User;
+import com.korit.allways_back.jwt.JwtTokenProvider;
 import com.korit.allways_back.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody SignupRequestDto signupRequestDto) {
         // DB 저장
         User registeredUser = userService.registerNewUser(signupRequestDto);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("accessToken", jwtTokenProvider.createToken(registeredUser));
+        response.put("message", "회원가입 완료");
+
         // 확인용
         System.out.println(registeredUser.getUserId());
 
-        return ResponseEntity.ok().body("회원가입이 완료");
+        return ResponseEntity.ok().body(response);
     }
 }
