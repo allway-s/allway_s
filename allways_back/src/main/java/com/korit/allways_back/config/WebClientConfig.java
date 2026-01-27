2590e9af897194ee126c25f76bb57e947c8145a4
HWANGDEOK <dukjun2002@naver.com>
2026-01-27 16:59:53 +0900

    feat(global): 카카오결제 기본 구조만 구현

---
+++ allways_back/src/main/java/com/korit/allways_back/config/WebClientConfig.java
@@ -0,0 +1,19 @@
+package com.korit.allways_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient kakaoWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://open-api.kakaopay.com")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+}
